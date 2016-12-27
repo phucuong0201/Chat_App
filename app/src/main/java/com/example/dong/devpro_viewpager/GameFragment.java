4daaff1b427c8a89cd10c7d96f42ba3f5529fef1
phucuong0201 <phucuong0201@gmail.com>
2016-12-27 13:12:48 +0700

    Da nhan button khi bam nut

--- app/src/main/java/com/example/dong/devpro_viewpager/GameFragment.java
+++ app/src/main/java/com/example/dong/devpro_viewpager/GameFragment.java
@@ -1,6 +1,7 @@
 package com.example.dong.devpro_viewpager;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.HashMap;
 import java.util.Random;
 
 import java.util.ArrayList;
@@ -23,8 +26,8 @@ public class GameFragment extends Fragment {
     private Button btn7;
     private Button btn8;
     private Button btn9;
-    private ArrayList<CaroObject> arr_sol = new ArrayList<>();
-    private CaroObject caroObject;
+    private final int identifynum = random();
+    private ArrayList<String> arr_sol = new ArrayList<>();
 
     public GameFragment() {
     }
@@ -32,114 +35,110 @@ public class GameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        final int identify = random();
-//        caroObject.setIdent(identify);
-//
-//        btn1 = (Button)getView().findViewById(R.id.btn_1);
-//        btn2 = (Button)getView().findViewById(R.id.btn_2);
-//        btn3 = (Button)getView().findViewById(R.id.btn_3);
-//        btn4 = (Button)getView().findViewById(R.id.btn_4);
-//        btn5 = (Button)getView().findViewById(R.id.btn_5);
-//        btn6 = (Button)getView().findViewById(R.id.btn_6);
-//        btn7 = (Button)getView().findViewById(R.id.btn_7);
-//        btn8 = (Button)getView().findViewById(R.id.btn_8);
-//        btn9 = (Button)getView().findViewById(R.id.btn_9);
-//
-//
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                caroObject.setIdent(identify);
-//                caroObject.setLocate(1);
-//                arr_sol.add(caroObject);
-//                String test = String.valueOf(arr_sol.get(0).getIdent()) + String.valueOf(arr_sol.get(0).getLocate());
-//                btn1.setText("x");
-//                Toast.makeText(getContext(), "1", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        btn2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                caroObject.setIdent(identify);
-//                caroObject.setLocate(2);
-//                arr_sol.add(caroObject);
-//                btn2.setText("x");
-//                Toast.makeText(getContext(), "2", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(3);
-//                arr_sol.add(caroObject);
-//                btn3.setText("x");
-//                Toast.makeText(getContext(), "3", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(4);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "4", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn5.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(5);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "5", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn6.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(6);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "6", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn7.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(7);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "7", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn8.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(8);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "8", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        btn9.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                caroObject.setLocate(9);
-//                arr_sol.add(caroObject);
-//                btn4.setText("x");
-//                Toast.makeText(getContext(), "9", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view =  inflater.inflate(R.layout.fragment_game, container, false);
+        // Inflate the layout for this fragment
+        final int identify = identifynum;
+        btn1 = (Button)view.findViewById(R.id.btn_1);
+        btn2 = (Button)view.findViewById(R.id.btn_2);
+        btn3 = (Button)view.findViewById(R.id.btn_3);
+        btn4 = (Button)view.findViewById(R.id.btn_4);
+        btn5 = (Button) view.findViewById(R.id.btn_5);
+        btn6 = (Button) view.findViewById(R.id.btn_6);
+        btn7 = (Button) view.findViewById(R.id.btn_7);
+        btn8 = (Button) view.findViewById(R.id.btn_8);
+        btn9 = (Button) view.findViewById(R.id.btn_9);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn1" + "," + identify;
+                arr_sol.add(sol);
+                btn1.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn2" + "," + identify;
+                arr_sol.add(sol);
+                btn2.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn3" + "," + identify;
+                arr_sol.add(sol);
+                btn3.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn4" + "," + identify;
+                arr_sol.add(sol);
+                btn4.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn5" + "," + identify;
+                arr_sol.add(sol);
+                btn5.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn6" + "," + identify;
+                arr_sol.add(sol);
+                btn6.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn7" + "," + identify;
+                arr_sol.add(sol);
+                btn7.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn8" + "," + identify;
+                arr_sol.add(sol);
+                btn8.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sol = "btn9" + "," + identify;
+                arr_sol.add(sol);
+                btn9.setText("x");
+                Toast.makeText(getContext(), sol, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return view;
     }
 
     private final int random(){
@@ -147,4 +146,24 @@ public class GameFragment extends Fragment {
         int result = generator.nextInt(200 - 5) + 5;
         return result;
     }
+
+    private HashMap<String, Boolean> checkSol(ArrayList<String> array, int ident){
+        HashMap<String, Boolean> paint = new HashMap<>();
+        if (array.size() != 0){
+
+            for (String s : array) {
+                String[] split = s.split(",");
+                Boolean check = false;
+                if (split[1].equals(ident)){
+                    check = true;
+                }
+
+                paint.put(split[0], check);
+
+            }
+        }
+        return paint;
+    }
+
+
 }
